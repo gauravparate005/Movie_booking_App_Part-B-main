b82a0dca3e64abcf2a37c6b9a82ff16607759467
gauravparate005 <85149184+gauravparate005@users.noreply.github.com>
2021-09-14 02:49:31 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Home from './screens/Home/Home';
+
+
+ReactDOM.render(
+  <Home/>,
+  document.getElementById('root')
+);             
+
+
